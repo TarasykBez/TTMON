3a61804430560c6c0f479b71b680d10f1ff4376c
TarasykBez <tarasbezushko23@gmail.com>
2026-03-11 16:20:32 +0200

    Реалізація сторінки списку технічних завдань" -m "- Додано view для відображення всіх ТЗ з бази даних." -m "- Налаштовано URL-маршрути для додатку tasks." -m "- Створено шаблон task_list.html та інтегровано навігаційне меню для перемикання між Дашбордом і Завданнями.

--- TTMON/urls.py
+++ TTMON/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Робимо наш дашборд головною сторінкою всього сайту:
-    path('', include('analytics.urls')), 
+    path('', include('analytics.urls')), # Наш дашборд (головна сторінка)
+    path('tasks/', include('tasks.urls')), # Додаємо маршрут для ТЗ
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+
+def task_list(request):
+    # Отримуємо всі ТЗ, сортуючи від найновіших до найстаріших
+    tasks = Task.objects.all().order_by('-created_at')
+
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tasks/task_list.html', context)
